1efad8a9436d44935f2ae09361ad9a6f727cb111
CodingGoose <dbaraian650@gmail.com>
2026-03-01 17:36:39 +0200

    am creat fisierul pt problema 3 si mi-am organizat cerinta

---
+++ L1_Baraian_Dan_Problem3.cpp
@@ -0,0 +1,21 @@
+// ! ex asta (Problem 3) plus inca un exercitiu la alegere
+
+// Der Code muss gut lesbar sein, und wo notwendig Kommentare enthalten.
+
+//* Problem 3.
+
+/* * a. Lesen Sie eine Sequenz von natürlichen Zahlen (Sequenz mit 0 beendet)
+und bestimmen Sie die Anzahl von 0 Ziffern des Produkts der gelesenen Zahlen.
+*/
+
+
+
+/* *
+b. Gegeben sei ein Vektor von Zahlen, finde die längste zusammenhängende Teilfolge
+so, dass die Summe von zwei aufeinanderfolgenden Elementen eine Primzahl ist.
+*/
+
+int main() {
+
+    return 0;
+}
